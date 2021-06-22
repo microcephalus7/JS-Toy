6b001a626593384b7f961de28262f0a100100846
microcephalus7 <microcephalus7@gmail.com>
2021-06-22 22:47:21 +0900

    Create scope

---
+++ scope.js
@@ -0,0 +1,10 @@
+var microcephalus7 = "microcephalus7";
+function home() {
+  var microcephalus7 = "microcephalus7@gmail.com";
+  console.log(microcephalus7);
+  microcephalus7Call();
+}
+function microcephalus7Call() {
+  console.log(microcephalus7);
+}
+home();
---
+++ scope2.js
@@ -0,0 +1,10 @@
+let microcephalus7 = "microcephalus7";
+function home() {
+  let microcephalus7 = "microcephalus7@gmail.com";
+  console.log(microcephalus7);
+  microcephalus7Call();
+}
+function microcephalus7Call() {
+  console.log(microcephalus7);
+}
+home();
